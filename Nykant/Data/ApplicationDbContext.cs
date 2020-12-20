d92a1ab09fc88aa874e2e355ab27aff18c55bb96
Christian Salgård Svinding <43539294+chri30cm@users.noreply.github.com>
2020-12-20 17:07:26 +0100

    Add project files.

---
+++ Nykant/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nykant.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Nykant/wwwroot/JS/downarrowstop.js
@@ -0,0 +1,5 @@
+﻿$(window).scroll(function () {
+    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+        $("#downarrow").hide();
+    }
+});
