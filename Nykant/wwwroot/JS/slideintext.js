0d5a8a281a3600553b0a386230d2b24d19221551
Christian Salgård Svinding <43539294+chri30cm@users.noreply.github.com>
2021-01-10 19:06:08 +0100

    front end

---
+++ Nykant/wwwroot/JS/slideintext.js
@@ -0,0 +1,17 @@
+﻿var x = document.getElementById("slideMeIn");
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+        x.style.display = "block";
+    }
+});
+
+var u = document.getElementById("slideme");
+var y = document.getElementById("slide-container");
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 700) {
+        y.style.display = "block";
+        u.style.display = "block";
+    }
+});
+
+
--- Nykant/wwwroot/JS/slideshow.js
+++ Nykant/wwwroot/JS/slideshow.js
@@ -13,3 +13,4 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 7000); // Change image every 2 seconds
 }
+
