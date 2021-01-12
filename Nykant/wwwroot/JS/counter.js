9b84b2dd1aef7dfeb2449ca6634565d84ae3620c
Christian Salgård Svinding <43539294+chri30cm@users.noreply.github.com>
2021-01-12 16:25:03 +0100

    mere af det gode...

---
+++ Nykant/wwwroot/JS/counter.js
@@ -0,0 +1,12 @@
+﻿var textnumber = document.getElementById("quantitynumber");
+function countup() {
+    var number = parseInt(textnumber.textContent);
+    var result = number + 1;
+    document.getElementById("quantitynumber").textContent = result;
+}
+
+function countdown() {
+    var number = parseInt(textnumber.textContent);
+    var result = number - 1;
+    document.getElementById("quantitynumber").textContent = result;
+}
---
+++ Nykant/wwwroot/JS/slidedetails.js
@@ -0,0 +1,6 @@
+﻿var x = document.getElementById("detailsdescription");
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+        x.style.display = "block";
+    }
+});
