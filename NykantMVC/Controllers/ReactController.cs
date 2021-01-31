9aa1f819c16741494195e1fe31a91e183a5a78fb
Christian Salgård Svinding <43539294+chri30cm@users.noreply.github.com>
2021-01-31 08:59:31 +0100

    React JSX

---
+++ NykantMVC/Controllers/ReactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NykantMVC.Controllers
+{
+    public class ReactController : Controller
+    {
+        public IActionResult Tutorial()
+        {
+            return View();
+        }
+    }
+}
--- NykantMVC/Startup.cs
+++ NykantMVC/Startup.cs
@@ -13,6 +13,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.IdentityModel.Tokens.Jwt;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
 
 namespace NykantMVC
 {
@@ -51,6 +54,13 @@ namespace NykantMVC
                     options.Scope.Add("offline_access");
                 });
 
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+
+            // Make sure a JS engine is registered, or you will get an error!
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+              .AddV8();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
@@ -71,6 +81,27 @@ namespace NykantMVC
             }
 
             app.UseHttpsRedirection();
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/js/First.jsx")
+                //  .AddScript("~/js/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+            });
+
             app.UseStaticFiles();
 
             app.UseRouting();
