fe4789e15a1e4429c0eef697447bc8fd118889e2
Christian Salgård Svinding <43539294+chri30cm@users.noreply.github.com>
2021-02-04 00:48:53 +0100

    nice

---
+++ NykantMVC/wwwroot/JS/Inputstyling.js
@@ -0,0 +1,47 @@
+﻿var e = document.getElementById("input");
+var r = document.getElementById("label");
+var t = document.getElementById("underline");
+//function invalid() {
+//    if (e.value == "") {
+//        r.style.transform = "translate(-200px, 0px)";
+//        r.style.fontSize = "16px";
+//        r.style.opacity = "1";
+//        r.style.color = "grey";
+//        t.style.transform = "scaleX(0.2)";
+//        t.style.background = "grey";
+//    }
+//    else if (!e.value == "" || e.querySelector(":valid") || e.querySelector(":focus") || e.querySelector(":hover")) {
+//        r.style.transform = "translate(-200px, -25px)";
+//        r.style.fontSize = "12px";
+//        r.style.opacity = "0.5";
+//        r.style.color = "white";
+//        t.style.transform = "scaleX(1)";
+//        t.style.background = "white";
+//    }
+//}
+//function mouseover() {
+//    r.style.transform = "translate(-200px, -25px)";
+//    r.style.fontSize = "12px";
+//    r.style.opacity = "0.5";
+//    r.style.color = "white";
+//    t.style.transform = "scaleX(1)";
+//    t.style.background = "white";
+//}
+//function mouseout() {
+//    if (e.value == "" ) {
+//        r.style.transform = "translate(-200px, 0px)";
+//        r.style.fontSize = "16px";
+//        r.style.opacity = "1";
+//        r.style.color = "grey";
+//        t.style.transform = "scaleX(0.2)";
+//        t.style.background = "grey";
+//    }
+//}
+//function onfocus() {
+//    r.style.transform = "translate(-200px, -25px)";
+//    r.style.fontSize = "12px";
+//    r.style.opacity = "0.5";
+//    r.style.color = "white";
+//    t.style.transform = "scaleX(1)";
+//    t.style.background = "white";
+//}
---
+++ NykantMVC/wwwroot/JS/onloadAnimations.js
@@ -0,0 +1,15 @@
+﻿
+function onload() {
+    var e = document.getElementById("breadcrumb");
+    var size = 0;
+    var id = setInterval(frame, 1);
+    function frame() {
+        if (size == 1) {
+            clearInterval(id);
+        }
+        else {
+            size = size + 0.01; 
+            e.style.transform = `scaleX(${size})`;
+        }
+    }
+}
