4863afcff35647424c9207f6e4457b8151bdbfe3
Christian Salgård Svinding <43539294+Salgaar@users.noreply.github.com>
2021-09-03 13:41:49 +0200

    sdf

--- NykantIS/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ NykantIS/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -1,55 +1,55 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
-using NykantIS.Models;
+﻿//using Microsoft.AspNetCore.Authorization;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.UI.Services;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.AspNetCore.WebUtilities;
+//using NykantIS.Models;
 
-namespace NykantIS.Areas.Identity.Pages.Account
-{
-    [AllowAnonymous]
-    public class RegisterConfirmationModel : PageModel
-    {
-        private readonly UserManager<ApplicationUser> _userManager;
+//namespace NykantIS.Areas.Identity.Pages.Account
+//{
+//    [AllowAnonymous]
+//    public class RegisterConfirmationModel : PageModel
+//    {
+//        private readonly UserManager<ApplicationUser> _userManager;
 
-        public RegisterConfirmationModel(UserManager<ApplicationUser> userManager)
-        {
-            _userManager = userManager;
-        }
-        public bool Logout { get; set; }
+//        public RegisterConfirmationModel(UserManager<ApplicationUser> userManager)
+//        {
+//            _userManager = userManager;
+//        }
+//        public bool Logout { get; set; }
 
-        public string Email { get; set; }
+//        public string Email { get; set; }
 
-        public string ReturnUrl { get; set; }
+//        public string ReturnUrl { get; set; }
 
-        public string Status { get; set; }
+//        public string Status { get; set; }
 
-        public bool DisplayConfirmAccountLink { get; set; }
+//        public bool DisplayConfirmAccountLink { get; set; }
 
-        public string EmailConfirmationUrl { get; set; }
+//        public string EmailConfirmationUrl { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(string email, bool logout)
-        {
-            if (email == null)
-            {
-                return RedirectToPage("/Index");
-            }
+//        public async Task<IActionResult> OnGetAsync(string email, bool logout)
+//        {
+//            if (email == null)
+//            {
+//                return RedirectToPage("/Index");
+//            }
 
-            var user = await _userManager.FindByEmailAsync(email);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with email '{email}'.");
-            }
+//            var user = await _userManager.FindByEmailAsync(email);
+//            if (user == null)
+//            {
+//                return NotFound($"Unable to load user with email '{email}'.");
+//            }
 
-            Email = email;
-            Status = $"A confirmation email has been sent to your account: {email}, " +
-                                 $"before you can log in you have to press the confirmation link in that email";
+//            Email = email;
+//            Status = $"A confirmation email has been sent to your account: {email}, " +
+//                                 $"before you can log in you have to press the confirmation link in that email";
 
-            Logout = logout;
-            return Page();
-        }
-    }
-}
+//            Logout = logout;
+//            return Page();
+//        }
+//    }
+//}
