e2f9bd3cadb121c9061e9b8d045876d934cf12d2
Christian <Christian@DESKTOP-GB0B2FU>
2021-10-27 15:43:54 +0200

    sadf

---
+++ NykantMVC/wwwroot/JS/carousel.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    if (window.innerWidth > 768) {
+        $('.owl-carousel').slick({
+            infinite: true,
+            centerMode: false,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+        });
+    }
+    else {
+        $('.owl-carousel').slick({
+            infinite: true,
+            centerMode: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+        });
+    }
+});
+
