0cd16418cf0ab8165a993f1a56ccec92f9539744
Christian Salgård Svinding <43539294+Salgaar@users.noreply.github.com>
2022-01-24 11:50:36 +0100

    rettet produkt tekster, og andre tekster.

---
+++ NykantAPI/Migrations/20220124093411_ThyraShort.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NykantAPI.Migrations
+{
+    public partial class ThyraShort : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "Description", "Materials", "Note", "Package", "Size" },
+                values: new object[] { "Thyra Short Description", "Thyra Short Materials", "Thyra Short Note", "Thyra Short Package", "Thyra Short Size" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "Description", "Materials", "Note", "Package", "Size" },
+                values: new object[] { "Thyra Description", "Thyra Materials", "Thyra Note", "Thyra Package", "Thyra Size" });
+        }
+    }
+}
