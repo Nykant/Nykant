de2d0f5ace228450a431f196a74f50825ec2c511
Christian <Christian@DESKTOP-GB0B2FU>
2022-02-25 11:15:40 +0100

    produkt beskrivelse ny, produkt data opdateret

--- NykantAPI/Data/migrations/20220225101445_ProductOpdatering.Designer.cs
+++ NykantAPI/Data/migrations/20220225101445_ProductOpdatering.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NykantAPI.Data;
 
 namespace NykantAPI.data.migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220225101445_ProductOpdatering")]
+    partial class ProductOpdatering
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- NykantMVC/wwwroot/JS/product-details.js
+++ NykantMVC/wwwroot/JS/product-details.js
@@ -1,26 +1,48 @@
 ﻿
 
-var contents = document.getElementsByClassName("collapsible-content");
-var i, j;
+//var contents = document.getElementsByClassName("collapsible-content");
+//var i, j;
 
-for (j = 0; j < contents.length; j++) {
-    var content = contents[j];
-    content.style.maxHeight = content.scrollHeight + "px";
-}
+//for (j = 0; j < contents.length; j++) {
+//    var content = contents[j];
+//    content.style.maxHeight = content.scrollHeight + "px";
+//}
 
-var coll2 = document.getElementsByClassName("collapsible-button2");
+//var coll2 = document.getElementsByClassName("collapsible-button2");
 
-for (var i = 0; i < coll2.length; i++) {
-    coll2[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
+//for (var i = 0; i < coll2.length; i++) {
+//    coll2[i].addEventListener("click", function () {
+//        this.classList.toggle("active");
+//        var content = this.nextElementSibling;
+//        if (content.style.maxHeight) {
+//            content.style.maxHeight = null;
+//        } else {
+//            content.style.maxHeight = content.scrollHeight + "px";
+//        }
+//    });
+//}
+
+
+var sidestep_bodies = document.getElementById('sidestep-bodies').children;
+var sidestep_headers = document.getElementById('sidestep-headers').children;
+
+for (var i = 0; i < sidestep_headers.length; i++) {
+    sidestep_headers[i].addEventListener('click', function (event) {
+        for (var j = 0; j < sidestep_headers.length; j++) {
+            sidestep_headers[j].removeAttribute('class');
+            sidestep_bodies[j].removeAttribute('class');
+        }
+        event.target.setAttribute('class', 'active');
+        var number = parseInt(event.target.getAttribute("data-number"));
+        if (number == 3) {
+            sidestep_bodies[number - 1].setAttribute('class', 'active text-center');
+        }
+        else {
+            sidestep_bodies[number - 1].setAttribute('class', 'active');
         }
     });
 }
+
 
 var select = document.getElementById('length-select');
 if (select != undefined) {
