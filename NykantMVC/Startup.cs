e1ebda4975071896f416be97dd3657459f8b6f4e
Christian Salgård Svinding <43539294+Salgaar@users.noreply.github.com>
2022-03-29 12:53:41 +0200

    ingen personlig data i order sent, session data forlænget levetid, e-mærket plugin tilføjet

--- NykantMVC/Startup.cs
+++ NykantMVC/Startup.cs
@@ -115,7 +115,7 @@ namespace NykantMVC
 
             services.AddSession(options =>
             {
-                options.IdleTimeout = TimeSpan.FromHours(10);
+                options.IdleTimeout = TimeSpan.FromDays(30);
                 options.Cookie.HttpOnly = true;
                 options.Cookie.Name = "Session";
                 options.Cookie.IsEssential = true;
