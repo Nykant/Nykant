ed04de2f81bc8d43e424e99511af3c571d91474e
Christian <Christian@DESKTOP-GB0B2FU>
2022-05-06 14:20:41 +0200

    moving box

---
+++ NykantMVC/wwwroot/JS/moving-box.js
@@ -0,0 +1,15 @@
+﻿
+
+$("#follow-container").mousemove(function (e) {
+    // e = Mouse click event.
+
+    var rect = e.target.getBoundingClientRect();
+
+    var x = e.clientX - rect.left; //x position within the element.
+    var y = e.clientY - rect.top;  //y position within the element.
+
+    $("#follow-box").css('left', x);
+    $("#follow-box").css('top', y);
+
+/*    $("#follow-box").animate({ left: x - 40, top: y - 40 }, 1, 'linear');*/
+});
