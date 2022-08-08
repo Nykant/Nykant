b57dfe2d5ae445d9694d8ceedef711c01535f3af
Salgaar <43539294+Salgaar@users.noreply.github.com>
2022-08-08 15:52:22 +0200

    asd

--- NykantAPI/Startup.cs
+++ NykantAPI/Startup.cs
@@ -66,27 +66,27 @@ namespace NykantAPI
             }
 
             //migrations to mysql server
-            services.AddDbContext<ApplicationDbContext>(
-                options =>
-                {
-                    options.UseMySql(
-                        nykantConnection,
-                        x => x.MigrationsAssembly("MySqlMigrations"));
-                });
-
             //services.AddDbContext<ApplicationDbContext>(
-            //    options => _ = provider switch
+            //    options =>
             //    {
-            //        "SqlServer" => options.UseSqlServer(
-            //            nykantConnectionLocal,
-            //            x => x.MigrationsAssembly("SqlServerMigrations")),
-
-            //        "MySql" => options.UseMySql(
+            //        options.UseMySql(
             //            nykantConnection,
-            //            x => x.MigrationsAssembly("MySqlMigrations")),
-
-            //        _ => throw new Exception($"Unsupported provider: {provider}")
+            //            x => x.MigrationsAssembly("MySqlMigrations"));
             //    });
+
+            services.AddDbContext<ApplicationDbContext>(
+                options => _ = provider switch
+                {
+                    "SqlServer" => options.UseSqlServer(
+                        nykantConnectionLocal,
+                        x => x.MigrationsAssembly("SqlServerMigrations")),
+
+                    "MySql" => options.UseMySql(
+                        nykantConnection,
+                        x => x.MigrationsAssembly("MySqlMigrations")),
+
+                    _ => throw new Exception($"Unsupported provider: {provider}")
+                });
 
 
 
