82cfbcfdffddfcc1abf9bca6efd2f196b17e7295
Salgaar <43539294+Salgaar@users.noreply.github.com>
2022-10-31 13:32:19 +0100

    sdfg

--- NykantMVC/Services/EmailService.cs
+++ NykantMVC/Services/EmailService.cs
@@ -189,7 +189,7 @@ namespace NykantMVC.Services
                 smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
                 smtp.Authenticate(_mailSettings.Username, _mailSettings.Password);
                 await smtp.SendAsync(email);
-                _logger.LogError($"DKI EMAIL: sent");
+                _logger.LogInformation($"DKI EMAIL: sent");
                 smtp.Disconnect(true);
                 return "success";
             }
